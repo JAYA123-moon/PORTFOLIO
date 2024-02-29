b1b9aa53cb9bb21567b8c5731e54b35c32418d62
JAYA123-moon <136203707+JAYA123-moon@users.noreply.github.com>
2024-02-29 22:21:04 +0530

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["MernStack Developer . . ." , "Programmer . . ." , "Web Developer . . ." ,"Adobe Photoshop & Premiere Pro Expert. . ."  ],
+    typeSpeed: 65,
+    backSpeed: 50,
+    backDelay: 1200,
+    loop: true
+})
